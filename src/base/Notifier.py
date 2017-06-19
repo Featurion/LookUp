83009dbbb2fd94e222b254dd80639c5190a1d25d
Zach Gates <thezachgates@gmail.com>
2017-06-19 17:00:04 -0500

    Revert "notifier: Remove ugly spaces."
    This reverts commit 04d464965bd740b0c3e8ef88aa30a709994fc689.

--- src/base/Notifier.py
+++ src/base/Notifier.py
@@ -57,7 +57,7 @@ class Notifier(object):
             self.__channel = channel
 
         def format(self, msg_type, msg):
-            return '[{0}] {1}: {2}'.format(msg_type.upper(),
+            return '[{0}] {1}: {2}'.format(msg_type.upper().ljust(8),
                                            self.__name,
                                            msg[:1].upper() + msg[1:])
 
